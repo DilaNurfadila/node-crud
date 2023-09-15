71dd90c11acdfc8750b59954ef09e7973d7cff5c
DilaNurfadila <nurfadila1523@gmail.com>
2023-09-15 21:04:02 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const bp = require("body-parser");
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_crud_db",
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected!");
+});
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+app.use(bp.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  // Menampilkan list
+  db.query("select * from kontak", (err, rows) => {
+    if (err) throw err;
+    res.render("index.ejs", { kontak: rows });
+  });
+});
+
+// not displaying
+app.get("/:id", (req, res) => {
+  // Menampilkan detail
+  const { id } = req.params;
+  const sql = `select * from kontak where id=${id}`;
+  db.query(sql, (err, row) => {
+    if (err) throw err;
+    res.render("detail.ejs", { kontak: row });
+  });
+});
+
+app.post("/", (req, res) => {
+  // Membuat data
+  //   console.log(req.body);
+  const { nama, nohp } = req.body;
+  const sql = `insert into kontak values(NULL, '${nama}', '${nohp}')`;
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+// not working
+app.get("/:id/edit", (req, res) => {
+  // Mengubah data
+  const { id } = req.params;
+  const sql = `select * from kontak where id=${id}`;
+  db.query(sql, (err, row) => {
+    if (err) throw err;
+    res.render("edit.ejs", { kontak: row });
+  });
+});
+
+// not working
+app.post("/:id/edit", (req, res) => {
+  // Menyimpan data
+  const { id } = req.params;
+  const { nama, nohp } = req.body;
+  const sql = `update kontak set nama='${nama}', nohp='${nohp}' where id=${id}`;
+  db.query(sql, (err, row) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+// not working
+app.delete("/:id/delete", (req, res) => {
+  // Menghapus data
+  const { id } = req.params;
+  const sql = `delete from kontak where id=${id}`;
+  db.query(sql, (err, rows) => {
+    if (err) throw err;
+    res.redirect("/");
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+  console.log(`http://localhost:${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Node CRUD - Not finished yet
+
+Problem :
+
+- Get detail (not displaying)
+
+- Edit data (not working)
+
+- Save data (not working)
+
+- Delete data (not working)
